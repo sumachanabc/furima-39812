32f8ff8c120e6458fa3aa0781865564cf3263401
sumachanabc <bike.cbrider@gmail.com>
2023-08-30 17:57:06 +0900

    バリデーション改善

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,14 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
+  # 名前(全角)のバリデーション
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角文字で入力してください" }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角文字で入力してください" }
+
+  # 名前カナ(全角)のバリデーション
+  validates :read_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力してください" }
+  validates :read_family_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力してください" }
+
+  # 生年月日のバリデーション
   validates :date_of_birth, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :read_first_name, presence: true
-  validates :read_family_name, presence: true
 end
