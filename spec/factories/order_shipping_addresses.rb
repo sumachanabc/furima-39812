028eacccbf3d5b67b5321005bbf4b09c6eca76ec
sumachanabc <bike.cbrider@gmail.com>
2023-09-05 15:53:26 +0900

    テストコード

--- spec/factories/order_shipping_addresses.rb
+++ spec/factories/order_shipping_addresses.rb
@@ -1,10 +1,14 @@
 FactoryBot.define do
-  factory :donation_address do
-    postal_code { '123-4567' }
-    prefecture { 2 }
-    city { '東京都' }
-    house_number { '1-1' }
-    building_name { '東京ハイツ' }
-    phone_number { Faker::PhoneNumber.subscriber_number(length: 10..11) }
+  factory :order_shipping_address do
+    association :user
+    association :item
+
+    postal_code    { '123-4567' }
+    prefecture_id  { 2 }
+    city           { '東京都' }
+    street_address { '1-1' }
+    building_name  { '東京ハイツ' }
+    phone_number   { '1234567890' }# { Faker::PhoneNumber.subscriber_number(number: 10..11) }
+    token { 'your_token_value_here' }
   end
 end
---
+++ spec/models/order_shipping_address_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe OrderShippingAddress, type: :model do
+  describe '商品購入機能' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item, user_id: user.id)
+      @order_shipping_address = FactoryBot.build(:order_shipping_address, user_id: user.id, item_id: item.id)
+      # sleep 0.1 # 0.1秒待機
+
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_shipping_address).to be_valid
+      end
+      it 'building_nameは空でも保存できること' do
+        @order_shipping_address.building_name = ''
+        expect(@order_shipping_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'tokenが空だと保存できないこと' do
+        @order_shipping_address.token = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'postal_codeが空だと保存できないこと' do
+        @order_shipping_address.postal_code = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @order_shipping_address.postal_code = '1234567'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+      it 'prefecture_idを選択していないと保存できないこと' do
+        @order_shipping_address.prefecture_id = 1
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include('Prefecture must be other than 1')
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @order_shipping_address.phone_number = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが9桁の場合はエラーになること' do
+        @order_shipping_address.phone_number = '123456789'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include('Phone number は10桁または11桁で入力してください')
+      end
+
+      it 'phone_numberが12桁の場合はエラーになること' do
+        @order_shipping_address.phone_number = '123456789012'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include('Phone number は10桁または11桁で入力してください')
+      end
+
+      it 'phone_numberが半角数字以外を含む場合はエラーになること' do
+        @order_shipping_address.phone_number = '090-1234-5678'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include('Phone number は半角数字のみ入力してください')
+      end
+
+      it 'userが紐付いていないと保存できないこと' do
+        @order_shipping_address.user_id = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐付いていないと保存できないこと' do
+        @order_shipping_address.item_id = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Item can't be blank")
+      end
+    end
+  end
+end
