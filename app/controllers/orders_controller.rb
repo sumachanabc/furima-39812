ad70cd022ff1a12888d74d59f5e589ee55e4d706
sumachanabc <bike.cbrider@gmail.com>
2023-09-05 14:12:21 +0900

    トークン決済実装完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :item_tokutei, only: [:index, :create]
+  before_action :item_tokutei, only: [:index, :create, :pay_item]
 
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"] # 環境変数を使用して秘密鍵を代入。RailsからJavaScriptへ公開鍵を渡す
@@ -14,12 +14,14 @@ class OrdersController < ApplicationController
   def create
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"] # 環境変数を使用して秘密鍵を代入。RailsからJavaScriptへ公開鍵を渡す
     @order_shipping_address = OrderShippingAddress.new(order_params)
-    if @order_shipping_address.valid?# valid?メソッドを使用しているのは、OrderShippingAddressクラスがApplicationRecordを継承していないことにより、saveメソッドにはバリデーションを実行する機能がないため
+    # 条件分岐を追加して、要件に合わせたリダイレクトを行う
+    if user_can_purchase? && @order_shipping_address.valid?# valid?メソッドを使用しているのは、OrderShippingAddressクラスがApplicationRecordを継承していないことにより、saveメソッドにはバリデーションを実行する機能がないため
       pay_item
       @order_shipping_address.save
       redirect_to root_path
     else
-      render :index, status: :unprocessable_entity
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render :index, status: :unprocessable_entity# バリデーションに引っかかった場合は、indexアクションが実行されることなくindex.html.erbがrenderされるため、別途gonの設定が必要。
     end
   end
 
@@ -34,12 +36,23 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述
+    item_price = @item.price
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
+      amount: item_price,  # 商品の値段
+      card: order_params[:token],    # トークン化されたカード情報
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
+  end
+
+  def user_can_purchase?
+    if current_user == @item.user# 購入が許可されるかどうかの条件分岐を実装
+      false# 自身が出品した商品の場合はトップページにリダイレクト
+    elsif @item.sold?
+      false# 売却済み商品の場合はトップページにリダイレクト
+    else
+      true# ログアウト状態の場合はログインページにリダイレクト
+    end
   end
 
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -40,3 +40,4 @@ const pay = () => {
 };
 
 window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
